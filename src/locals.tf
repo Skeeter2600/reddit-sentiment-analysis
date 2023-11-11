58e70b7cf263b261d079cc24c762ab8bbe81ee9a
Samuel Lyle-Aldrich <ssl3380@rit.edu>
2023-11-10 20:28:59 -0500

    setup initial vue project

--- src/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  reddit_client_id      = "lqw-jyMMlY2h_czcIotjZg"
-  reddit_client_secret  = "fNERJaysCcVXz1VHt1je-VcziegvFA"
-}
--- src/output.tf
+++
@@ -1,4 +0,0 @@
-output "api_url" {
-  value         = aws_api_gateway_deployment.reddit_api_deployment.invoke_url
-  description   = "url used to invoke the api"
-}
